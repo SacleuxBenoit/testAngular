f268d3f2e45a82e12fb689a2ac4ec8c4e8b24e83
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-08 00:39:02 +0100

    add the import of newcompo in app.module.ts

--- testAngular/src/app/app.module.ts
+++ testAngular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewcompoComponent } from './newcompo/newcompo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewcompoComponent
   ],
   imports: [
     BrowserModule,
