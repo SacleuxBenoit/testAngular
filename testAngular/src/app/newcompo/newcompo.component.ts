7c76a9ff2237d65d0ea2c74bee4e015daa5a692d
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-08 14:59:41 +0100

    delete the useless code (just the h1 left)

--- testAngular/src/app/newcompo/newcompo.component.ts
+++ testAngular/src/app/newcompo/newcompo.component.ts
@@ -3,14 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-newcompo',
   template: `
-  <h1 [style.background-color]="colorClass">Bonjour à tous</h1>
+  <h1>Bonjour à tous</h1>
   `,
-  styles: [`.test-class{
-    color:crimson;
-  }`]
+  styles: []
 })
 export class NewcompoComponent implements OnInit {
-public colorClass="cyan";
+
   constructor() { }
 
   ngOnInit() {
