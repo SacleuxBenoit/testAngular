3865a5be6324b28009a1717f30a39a7c5f7371d4
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-08 13:40:15 +0100

    delete the class in the template for a new test in newcompo.component.ts

--- testAngular/src/app/newcompo/newcompo.component.ts
+++ testAngular/src/app/newcompo/newcompo.component.ts
@@ -3,14 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-newcompo',
   template: `
-  <h1 class="test-class">Bonjour à tous</h1>
+  <h1>Bonjour à tous</h1>
   `,
   styles: [`.test-class{
     color:crimson;
   }`]
 })
 export class NewcompoComponent implements OnInit {
-
+public redClass ="test-class";
   constructor() { }
 
   ngOnInit() {
