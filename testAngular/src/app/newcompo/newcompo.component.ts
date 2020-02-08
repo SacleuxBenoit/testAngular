e4a451ddc985a3c082660168e45188a8285143fc
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-08 15:25:37 +0100

    add else otherTitle to the h1 in newcompo.component.ts

--- testAngular/src/app/newcompo/newcompo.component.ts
+++ testAngular/src/app/newcompo/newcompo.component.ts
@@ -3,12 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-newcompo',
   template: `
-  <h1 *ngIf="showTitle">Bonjour à tous</h1>
+  <h1 *ngIf="showTitle; else otherTitle">Bonjour à tous</h1>
 `,
   styles: []
 })
 export class NewcompoComponent implements OnInit {
-  public showTitle=false
+  public showTitle=true
   constructor() { }
 
   ngOnInit() {
