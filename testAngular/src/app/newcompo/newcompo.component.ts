e16ed48a725c138822090098fa2582f5893d65a2
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-08 15:18:24 +0100

    delete the useless code (i'll see the structural directives)

--- testAngular/src/app/newcompo/newcompo.component.ts
+++ testAngular/src/app/newcompo/newcompo.component.ts
@@ -4,18 +4,12 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-newcompo',
   template: `
   <h1>Bonjour à tous</h1>
-  <input #myTxt type="text"/> <button (click)=logText(myTxt.value)>Envoyer</button>
 `,
   styles: []
 })
 export class NewcompoComponent implements OnInit {
-public Bienvenue="";
   constructor() { }
 
   ngOnInit() {
   }
-  logText(value){
-    console.log(value);
-  }
-
 }
