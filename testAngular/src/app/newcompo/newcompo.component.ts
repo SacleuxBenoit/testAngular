f764c32e9cd8d220538810dadc5f9d2807c3fc72
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-08 15:27:10 +0100

    add ng-template for try to add another title when showTitle is false in newcompo.component.ts

--- testAngular/src/app/newcompo/newcompo.component.ts
+++ testAngular/src/app/newcompo/newcompo.component.ts
@@ -4,11 +4,12 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-newcompo',
   template: `
   <h1 *ngIf="showTitle; else otherTitle">Bonjour à tous</h1>
+  <ng-template #otherTitle><h1>Test d'un autre titre</h1></ng-template>
 `,
   styles: []
 })
 export class NewcompoComponent implements OnInit {
-  public showTitle=true
+  public showTitle=false
   constructor() { }
 
   ngOnInit() {
