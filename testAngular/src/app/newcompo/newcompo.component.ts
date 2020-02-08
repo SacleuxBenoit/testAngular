edaba6bfa544a03925325a560ec5d3bd6949be99
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-08 15:24:06 +0100

    add public showTitle=false and add showTitle to the ngIF in newcompo.component.ts

--- testAngular/src/app/newcompo/newcompo.component.ts
+++ testAngular/src/app/newcompo/newcompo.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-newcompo',
   template: `
-  <h1 *ngIf="true">Bonjour à tous</h1>
+  <h1 *ngIf="showTitle">Bonjour à tous</h1>
 `,
   styles: []
 })
 export class NewcompoComponent implements OnInit {
+  public showTitle=false
   constructor() { }
 
   ngOnInit() {
