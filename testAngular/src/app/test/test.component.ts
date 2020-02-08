c1db02edb09cf61c33fb196e59aa35b949e8f460
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-08 16:47:52 +0100

    delete some useless code in test.component.ts

--- testAngular/src/app/test/test.component.ts
+++ testAngular/src/app/test/test.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: '',
+  styles: []
 })
 export class TestComponent implements OnInit {
 
