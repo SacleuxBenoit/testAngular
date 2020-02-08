6846fa83faeaf81be1747df30233a87218e3bd55
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-08 12:44:30 +0100

    change the h1 for adding an interpolation in newcompo.component.ts

--- testAngular/src/app/newcompo/newcompo.component.ts
+++ testAngular/src/app/newcompo/newcompo.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-newcompo',
   template: `
-  <h1>Un test avec un titre</h1>
+  <h1>Bonjour à toi {{name}}</h1>
   
   `,
   style: []
