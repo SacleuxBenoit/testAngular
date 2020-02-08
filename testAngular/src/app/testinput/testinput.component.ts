6df2bb7bc666b49dede5f11dbb9a07a8c3a004bd
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-08 21:05:45 +0100

    delete the useless code in testinput.component.ts

--- testAngular/src/app/testinput/testinput.component.ts
+++ testAngular/src/app/testinput/testinput.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-testinput',
-  templateUrl: './testinput.component.html',
-  styleUrls: ['./testinput.component.css']
+  template: '<h1>test</h1>',
+  styles: []
 })
 export class TestinputComponent implements OnInit {
 
