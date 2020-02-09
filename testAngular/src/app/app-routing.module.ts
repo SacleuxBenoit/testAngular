947aca0f682acb06fcb510c00a047daea7c04a0d
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-09 17:26:27 +0100

    add path to te component accueil in app-routing.module.ts

--- testAngular/src/app/app-routing.module.ts
+++ testAngular/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NewcompoComponent } from './newcompo/newcompo.component';
 import { PageNotfoundComponent } from './page-notfound/page-notfound.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 
 const routes: Routes = [
+  { path: "", component: AccueilComponent, pathMatch: "prefix"},
   { path: "newcompo", component: NewcompoComponent},
   { path: "**", component: PageNotfoundComponent}
 ];
