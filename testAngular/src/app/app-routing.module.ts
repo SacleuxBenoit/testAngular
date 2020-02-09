4a79a4b80d77297343ccf00f8d5d448e80ef6743
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-09 17:11:03 +0100

    add path newcompo in app-routing.module.ts

--- testAngular/src/app/app-routing.module.ts
+++ testAngular/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewcompoComponent } from './newcompo/newcompo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: "newcompo", component: NewcompoComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
