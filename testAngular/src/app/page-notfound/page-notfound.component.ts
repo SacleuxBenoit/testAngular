170e55d64c67656aa5218c740c5a44be115d05b5
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-09 17:22:46 +0100

    change the paragraph to the title and change the content in page-notfound.component.ts

--- testAngular/src/app/page-notfound/page-notfound.component.ts
+++ testAngular/src/app/page-notfound/page-notfound.component.ts
@@ -3,9 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-notfound',
   template: `
-    <p>
-      page-notfound works!
-    </p>
+    <h3>
+    Page 404, la page que vous recherchez n'existe pas !
+    </h3>
   `,
   styles: []
 })
