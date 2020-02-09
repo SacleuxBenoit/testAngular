28d4f277a0da3ba781c95d0d9a894b73e70b9d48
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-09 14:45:25 +0100

    add the categorie sexe in firends.service.ts

--- testAngular/src/app/friends.service.ts
+++ testAngular/src/app/friends.service.ts
@@ -9,9 +9,9 @@ export class FriendsService {
 
   getFriends(){
     return[
-      {name :"Justine", age:25},
-      {name :"Alex", age:27},
-      {name :"Pierre", age:31},
+      {name :"Justine", age:25, sexe:"Femme"},
+      {name :"Alex", age:27, sexe:"Homme"},
+      {name :"Pierre", age:31, sexe:"Homme"},
     ]
   }
 }
