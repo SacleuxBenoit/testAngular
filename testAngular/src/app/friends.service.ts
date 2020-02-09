25360471676e42ca1b9568500ca14822dc1f2fc5
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-09 16:42:20 +0100

    add httpClient to the constructor in friends.service.ts

--- testAngular/src/app/friends.service.ts
+++ testAngular/src/app/friends.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FriendsService {
 private _url:string = "/assets/data/friends.json"
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getFriends(){
     return
